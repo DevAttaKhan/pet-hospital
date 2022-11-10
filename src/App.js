0571c03ad097eb880e9755707554dc3e90393b50
DevAttaKhan <attakhan220@gmail.com>
2022-11-10 06:48:00 -0700

    good second commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from "react";
 import Home from "./Pages/Home/Home";
 
 import { Routes, Route } from "react-router-dom";
-
 import Footer from "./Components/Footer/Footer";
-
 import Header from "./Components/Header/Header";
 import TopBar from "./Components/TopBar/TopBar";
 
